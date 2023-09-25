536f976e33e27752067b561ebb50baa62035ab62
Juan Pablo Uriol Balbín <jpuriol@gmail.com>
2023-09-25 15:14:38 +0200

    Añadida barra de progreso mientras se cargan los datos

--- app/src/main/java/com/example/rickandmorty/ui/components/CharactersList.kt
+++ app/src/main/java/com/example/rickandmorty/ui/components/CharactersList.kt
@@ -1,10 +1,13 @@
 package com.example.rickandmorty.ui.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -13,8 +16,18 @@ fun CharactersList(
     navigateToDetail: (id: Int) -> Unit
 ) {
     LazyColumn(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        if (characters.isEmpty()) {
+            item {
+                CircularProgressIndicator()
+            }
+            return@LazyColumn
+        }
+
         items(characters) { character ->
             CharacterSummary(
                 character = character,
