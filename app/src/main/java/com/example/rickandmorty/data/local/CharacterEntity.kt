82393d2ba371afce1c8ecc2d402cd94885f03000
Juan Pablo Uriol Balbín <jpuriol@gmail.com>
2023-10-25 07:38:23 +0200

    Quitados valores por defecto en CharacterEntity

--- app/src/main/java/com/example/rickandmorty/data/local/CharacterEntity.kt
+++ app/src/main/java/com/example/rickandmorty/data/local/CharacterEntity.kt
@@ -6,13 +6,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "items")
 data class CharacterEntity(
-    @PrimaryKey val id: Int = 0,
-    val name: String = "",
-    val status: String = "",
-    val species: String = "",
-    val type: String = "",
-    val gender: String = "",
-    val origin: String = "",
-    val location: String = "",
-    val imageURL: String = "",
+    @PrimaryKey val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val origin: String,
+    val location: String,
+    val imageURL: String,
 )
